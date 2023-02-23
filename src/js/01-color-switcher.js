0f36330a1c1473726806bc2e636c5216f603c445
LiudmylaShevchuk <lashevchuk12@gmail.com>
2023-02-23 17:10:34 +0200

    01-color-switcher.js changed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+refs.btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.btnStart.setAttribute('disabled', true);
+});
+
+refs.btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.btnStart.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
