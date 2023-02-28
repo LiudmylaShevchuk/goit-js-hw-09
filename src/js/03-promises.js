842d5738f5d62a2f0805c03f2a634dbb085e3912
LiudmylaShevchuk <lashevchuk12@gmail.com>
2023-02-28 12:27:17 +0200

    03-promises.js updated

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+const refs = {
+  form: document.querySelector('.form'),
+};
+
+refs.form.addEventListener('submit', onCreatePromises);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
+}
+
+function onCreatePromises(e) {
+  e.preventDefault();
+  const formData = new FormData(e.currentTarget);
+  const dataParams = {};
+
+  for (const [key, value] of formData.entries()) {
+    dataParams[key] = Number(value);
+  }
+
+  let { amount, step, delay } = dataParams;
+
+  for (let i = 1; i <= amount; i += 1) {
+    delay += step;
+
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        alert(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        alert(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    refs.form.reset();
   }
 }
